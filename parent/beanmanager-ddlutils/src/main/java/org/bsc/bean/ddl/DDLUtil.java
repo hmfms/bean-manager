03251ec75759affd9dac38240c5f7da19cd43dac
bartolomeo.sorrentino <bartolomeo.sorrentino@0b31507d-7e48-0410-b1cc-adc57de51046>
2011-08-12 13:15:55 +0000

    add createTableModel utility
    git-svn-id: http://bean-manager.googlecode.com/svn/trunk@797 0b31507d-7e48-0410-b1cc-adc57de51046

--- parent/beanmanager-ddlutils/src/main/java/org/bsc/bean/ddl/DDLUtil.java
+++ parent/beanmanager-ddlutils/src/main/java/org/bsc/bean/ddl/DDLUtil.java
@@ -19,104 +19,122 @@ import org.bsc.bean.PropertyDescriptorPK;
 
 
 public class DDLUtil {
-	 /**
-	  * 
-	  * @param pd
-	  * @return
-	  */
-	 public static Column fromPropertyDescriptorToColumn( PropertyDescriptor pd ) {
-	        
-	        Column result = null;
-	        
-	        if( pd instanceof PropertyDescriptorField ) {
-	        
-	                if( pd instanceof PropertyDescriptorJoin ) return result;
-	        
-	                PropertyDescriptorField f = (PropertyDescriptorField)pd;
-	        
-	                result  = new Column();
-	        
-	                result.setName( f.getFieldName() );
-	                if( f.getSize()>0 ) result.setSizeAndScale( f.getSize(), 0 );
-	                result.setTypeCode( f.getSQLType() );
-	                result.setRequired( f.isRequided() );
-	                result.setPrimaryKey( f instanceof PropertyDescriptorPK );
-	        
-	        }       
-	        
-	        return result;
-	 }
-	        
-	 /**
+	/**
+	 * 
+	 * @param pd
+	 * @return
+	 */
+	public static Column fromPropertyDescriptorToColumn(PropertyDescriptor pd) {
+
+		Column result = null;
+
+		if (pd instanceof PropertyDescriptorField) {
+
+			if (pd instanceof PropertyDescriptorJoin)
+				return result;
+
+			PropertyDescriptorField f = (PropertyDescriptorField) pd;
+
+			result = new Column();
+
+			result.setName(f.getFieldName());
+			if (f.getSize() > 0)
+				result.setSizeAndScale(f.getSize(), 0);
+			result.setTypeCode(f.getSQLType());
+			result.setRequired(f.isRequided());
+			result.setPrimaryKey(f instanceof PropertyDescriptorPK);
+
+		}
+
+		return result;
+	}
+
+	/**
 	 * Create a Table definition from bean info
+	 * 
 	 * @param info
 	 * @return Table definition
 	 */
-	 public static Table fromBeanInfoToTable( BeanInfo info ) {
-	                
-	        Table t = new Table();
-	        
-	        Index primaryIndex = new UniqueIndex();
-	        
-	        BeanDescriptor bd = info.getBeanDescriptor();
-	                
-	        String name = BeanDescriptorUtils.getEntityName(bd);
+	public static Table fromBeanInfoToTable(BeanInfo info) {
 
-	        if( null==name ) throw new IllegalArgumentException( "info is not a vaid beanManager beanInfo!");
-	                
-	        t.setName(name);
-	        primaryIndex.setName(name.concat("_pidx"));
-	        
-	                         
-	        for( PropertyDescriptor pd : info.getPropertyDescriptors() ) {
+		Table t = new Table();
 
-	                Column c = fromPropertyDescriptorToColumn( pd );
-	                        
-	                if( null!=c ) {
-	                	if( c.isPrimaryKey() ) {
-	                		primaryIndex.addColumn( new IndexColumn(c) );
-	                	}
-	                    t.addColumn(c);
-	                }
-	        }
-	        
-	        t.addIndex(primaryIndex);
-	        
-	        return t;
-	 }
-	      
-	 	/**
-	 	 * 
-	 	 * @param db
-	 	 * @param Driver
-	 	 * @param connectionUrl
-	 	 * @return
-	 	 */
-		public static String generateSQL(Database db, String Driver, String connectionUrl) {
-		    
-		    Platform platform = PlatformFactory.createNewPlatformInstance(Driver.trim(), connectionUrl);
-		
-		    return platform.getCreateTablesSql(db, true /* drop table first*/, true /* continue on error */);
+		Index primaryIndex = new UniqueIndex();
 
-		    //
-		    // ADD INITIALIZATION DATA TO SQL
-		    //
-		    /*
-		    SqlDynaBean bean = new SqlDynaBean( SqlDynaClass.newInstance( db.findTable("AUDIT_FILTERS")));
-		    	
-		    	for( ConfigObject o : getInitData() ) {
-		    	
-		    		BeanUtils.copyProperties(bean, o);
-		        
-		    		bean.set( "selected", (o.isSelected()) ? 'Y' : 'N' );
-		    		bean.set( "processed", (o.isProcessed()) ? 'Y' : 'N' );
-		    		System.out.println(
-		                	platform.getInsertSql(db, bean)
-		                	);
-		    		
-		    	}
+		BeanDescriptor bd = info.getBeanDescriptor();
+
+		String name = BeanDescriptorUtils.getEntityName(bd);
+
+		if (null == name)
+			throw new IllegalArgumentException(
+					"info is not a vaid beanManager beanInfo!");
+
+		t.setName(name);
+		primaryIndex.setName(name.concat("_pidx"));
+
+		for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+
+			Column c = fromPropertyDescriptorToColumn(pd);
+
+			if (null != c) {
+				if (c.isPrimaryKey()) {
+					primaryIndex.addColumn(new IndexColumn(c));
+				}
+				t.addColumn(c);
 			}
-			*/
 		}
+
+		t.addIndex(primaryIndex);
+
+		return t;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public static void createTablesModel(Database db, BeanInfo... infos) {
+
+		for (BeanInfo info : infos) {
+
+			db.addTable(fromBeanInfoToTable(info));
+
+		}
+	}
+
+	/**
+	 * 
+	 * @param db
+	 * @param Driver
+	 * @param connectionUrl
+	 * @return
+	 */
+	public static String generateSQL(Database db, String Driver,
+			String connectionUrl) {
+
+		Platform platform = PlatformFactory.createNewPlatformInstance(
+				Driver.trim(), connectionUrl);
+
+		return platform.getCreateTablesSql(db, true /* drop table first */,
+				true /* continue on error */);
+
+		//
+		// ADD INITIALIZATION DATA TO SQL
+		//
+		/*
+		 * SqlDynaBean bean = new SqlDynaBean( SqlDynaClass.newInstance(
+		 * db.findTable("AUDIT_FILTERS")));
+		 * 
+		 * for( ConfigObject o : getInitData() ) {
+		 * 
+		 * BeanUtils.copyProperties(bean, o);
+		 * 
+		 * bean.set( "selected", (o.isSelected()) ? 'Y' : 'N' ); bean.set(
+		 * "processed", (o.isProcessed()) ? 'Y' : 'N' ); System.out.println(
+		 * platform.getInsertSql(db, bean) );
+		 * 
+		 * } }
+		 */
+	}
 
 }
